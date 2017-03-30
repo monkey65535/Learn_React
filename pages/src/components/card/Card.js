eaf50bd9d0f2350439966e0453fec4d6be60bcbb
dilidili <466311467@qq.com>
2017-03-30 22:43:27 +0800

    prosinfo

--- pages/src/components/card/Card.js
+++ pages/src/components/card/Card.js
@@ -1,3 +1,18 @@
 /**
  * Created by Artoria on 2017/3/30 0030.
  */
+
+import React from 'react';
+
+class Card extends  React.Component {
+    render() {
+        console.log(this.props);
+        return(
+            <div className="card">
+                {this.props.children}
+            </div>
+        );
+    }
+}
+
+export default Card;
--- pages/src/index.js
+++ pages/src/index.js
@@ -8,20 +8,27 @@ import ComponentFooter from './components/Footer/Footer';
 import Lift from './components/Life/Life';
 import UseState from './components/UseState/UseState';
 import Lists from './components/lists/lists';
+import Card from './components/card/Card';
 
 
-class Index extends React.Component{
-  render() {
-    return (
-      <div id="app">
-        <ComponentHeader/>
-        <Lift/>
-        <UseState/>
-        <ComponentFooter/>
-        <Lists/>
-      </div>
-      )
-  }
+class Index extends React.Component {
+    render() {
+        return (
+            <div id="app">
+                <ComponentHeader/>
+                <Lift/>
+                <UseState/>
+                <ComponentFooter/>
+                <Lists/>
+                <Card>
+                    <div>
+                        <h2>React.js 小书</h2>
+                        <div>开源、免费、专业、简单</div>
+                        订阅：<input />
+                    </div>
+                </Card>
+            </div>
+        )
+    }
 }
-
-ReactDOM.render(<Index/>,document.getElementById('root'));
+ReactDOM.render(<Index/>, document.getElementById('root'));
