1845978332c5427074a28665de45371daa544ff5
dilidili <466311467@qq.com>
2017-07-30 20:37:13 +0800

    move

--- default-message/src/App.js
+++ default-message/src/App.js
@@ -6,7 +6,7 @@ import Nav from 'nav/Nav.js';
 import CardWrap from 'cardWrap/CardWrap.js';
 import Home from 'home/Home.js';
 
-require('../semantic/dist/semantic.css');
+require('../semantic/dist/semantic.min.css');
 require('./common/style/main.css');
 
 let data = [
